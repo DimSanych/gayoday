ba1f22e7df579dc512fc2c0aae42eb8c97131188
DimSanych <einwindir1488@gmail.com>
2023-08-22 20:12:04 +0300

    1

--- gayoday.py
+++ gayoday.py
@@ -40,6 +40,6 @@ if __name__ == "__main__":
 
 # Функция приветствия при добавлении бота в группу
 async def greet_new_members(update: Update, context) -> None:
-    for user in update.message.new_chat_members:
+    for user in update.message.NEW_CHAT_MEMBERS:
         if user.username == context.bot.username:
             await update.message.reply_text("Я искал пидрильный клуб любителей пощекотать очко и похоже я его нашел! Всем привет!")
