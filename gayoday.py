1daad860b3a3f05943614c75d01dc72c5f8c24f2
DimSanych <einwindir1488@gmail.com>
2023-08-22 20:19:02 +0300

    Update gayoday.py

--- gayoday.py
+++ gayoday.py
@@ -27,7 +27,7 @@ def main() -> None:
     # Регистрируем обработчики
     application.add_handler(CommandHandler("start", start))
     #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, greet_new_members))
+    #application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, greet_new_members))
 
 
 
