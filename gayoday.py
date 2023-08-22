ebb2838d9e07a4824828abaf7365aff60b7d9ac0
DimSanych <einwindir1488@gmail.com>
2023-08-22 18:29:11 +0300

    1

--- gayoday.py
+++ gayoday.py
@@ -1,18 +1,34 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я пидрила дня!.')
+# Включаем логирование
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# Обработчик команды /start
+async def start(update: Update, context) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет, {user.mention_html()}!",
+    )
+
+# Обработчик для повторения отправленных сообщений
+async def echo(update: Update, context) -> None:
+    await update.message.reply_text(update.message.text)
 
 def main() -> None:
-    TOKEN = "6696148424:AAG6-hZc4c2SAEEJwpU5QSp5smdK77ijcGI"
-    updater =   Updater(token=TOKEN, use_context=True)
-    
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+    # Создаем экземпляр бота и передаем ему токен вашего бота
+    application = Application.builder().token("6696148424:AAG6-hZc4c2SAEEJwpU5QSp5smdK77ijcGI").build()
 
-    updater.start_polling()
-    updater.idle()
+    # Регистрируем обработчики
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-if __name__ == '__main__':
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == "__main__":
     main()
