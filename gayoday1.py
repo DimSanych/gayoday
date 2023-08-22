fa115a94f4526c2d1d9f3a2db0bd30e5e564d792
DimSanych <einwindir1488@gmail.com>
2023-08-22 20:31:25 +0300

    1

--- gayoday1.py
+++ gayoday1.py
@@ -2,6 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
+
 # Включаем логирование
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -15,11 +16,9 @@ async def start(update: Update, context) -> None:
         rf"Привет, {user.mention_html()}!",
     )
 
-# Функция приветствия при добавлении бота в группу
-async def greet_new_members(update: Update, context) -> None:
-    for user in update.message.new_chat_members:
-        if user.username == context.bot.username:
-            await update.message.reply_text("Привет! Я здесь, чтобы помочь!")
+# Обработчик для повторения отправленных сообщений
+#async def echo(update: Update, context) -> None:
+#    await update.message.reply_text(update.message.text)
 
 def main() -> None:
     # Создаем экземпляр бота и передаем ему токен вашего бота
@@ -27,10 +26,21 @@ def main() -> None:
 
     # Регистрируем обработчики
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, greet_new_members))
+    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    #application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, greet_new_members))
+
+
 
     # Запускаем бота
     application.run_polling()
 
 if __name__ == "__main__":
     main()
+
+
+
+# Функция приветствия при добавлении бота в группу
+# async def greet_new_members(update: Update, context) -> None:
+#     for user in Update.message.NEW_CHAT_MEMBERS:
+#         if user.username == context.bot.username:
+#             await Update.message.reply_text("Я искал пидрильный клуб любителей пощекотать очко и похоже я его нашел! Всем привет!")
